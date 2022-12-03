77684054a4519068e691b086afd96713d7aedcfc
Mehroj-SK <115388393+Mehroj-SK@users.noreply.github.com>
2022-12-04 01:09:00 +0530

    Add files via upload

---
+++ tuplePractice_7.py
@@ -0,0 +1,10 @@
+var1 = ("hello")
+print(type(var1))  # <class 'str'>
+
+# Creating a tuple having one element
+var2 = ("hello",)
+print(type(var2))  # <class 'tuple'>
+
+# Parentheses is optional
+var3 = "hello",
+print(type(var3))  # <class 'tuple'>
